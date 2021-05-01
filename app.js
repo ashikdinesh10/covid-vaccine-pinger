e436c20c1c78fc79c4ee7689d08dd34244a29c23
Ashik Dinesh <dineshashik9@gmail.com>
2021-05-01 13:54:47 +0530

    covid pinger

---
+++ app.js
@@ -0,0 +1,62 @@
+const axios = require('axios')
+
+
+const iftttWebhookKey = 'gbnRBgvVWIbPNdp8zumwTBWKKf_LDeQNAQ6N65saW9b' // Replace value here
+const iftttWebhookName = 'notify' // Replace value here
+const districtId = '297'; // Replace value here
+const yourAge = '56'  // Replace value here
+
+
+const intervalInMs = 600000; // 15 mins interval (in milliseconds)
+const appointmentsListLimit = 3  // Increase/Decrease it based on the amount of information you want in the notification.
+
+function getDate() {
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+    const dd = tomorrow.getDate();
+    const mm = tomorrow.getMonth() + 1;
+    const yyyy = tomorrow.getFullYear();
+    return `${dd < 10 ? '0' + dd : dd}-${mm < 10 ? '0' + mm : mm}-${yyyy}`
+}
+const date = getDate();
+
+function pingCowin() {
+    axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtId}&date=${date}`).then((result) => {
+        const { centers }= result.data;
+        let isSlotAvailable = false;
+        let dataOfSlot = "";
+        let appointmentsAvailableCount = 0;
+        if(centers.length) {
+            centers.forEach(center => {
+                center.sessions.forEach((session => {
+                    if(session.min_age_limit < +yourAge && session.available_capacity > 0) {
+                        isSlotAvailable = true
+                        appointmentsAvailableCount++;
+                        if(appointmentsAvailableCount <= appointmentsListLimit) {
+                            dataOfSlot = `${dataOfSlot}\nSlot for ${session.available_capacity} is available: ${center.name} on ${session.date}`;
+                        }
+                    }
+                }))
+            });
+
+            dataOfSlot = `${dataOfSlot}\n${appointmentsAvailableCount - appointmentsListLimit} more slots available...`
+        }
+        if(isSlotAvailable) {
+            axios.post(`https://maker.ifttt.com/trigger/${iftttWebhookName}/with/key/${iftttWebhookKey}`, { value1: dataOfSlot }).then(() => {
+                console.log('Sent Notification to Phone \nStopping Pinger...')
+                clearInterval(timer);
+            });
+        }
+    }).catch((err) => {
+        console.log("Error: " + err.message);
+    });
+}
+
+let pingCount = 0;
+var timer = setInterval(() => {
+    console.clear();
+    pingCount += 1;
+    pingCowin();
+    console.log("Ping Count - ", pingCount);
+}, intervalInMs);
